b367fdc7b1f5b4886ee0c27f943c0ae663c030c1
Andrey Volkov <andreyka9678@mail.ru>
2022-02-22 02:47:58 +0500

    task2-change

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
-// 1 вариант
+// Задание 1
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min > max)
-  {return ('Задан неверный диапазон! Укажите другие числа.');}
-
+  if (min >= max || (max-min) < 0 || max < 0 || min < 0) {
+    return ('Задан неверный диапазон! Укажите другие числа.');
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomInt(1,9);
-// 2 вариант, тернарный оператор
-function getRandomSecond(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
 
-  return (min > max) ? 'Задан неверный диапазон! Укажите другие числа.' : Math.floor(Math.random() * (max - min + 1)) + min;
-}
-getRandomSecond(2,9);
 // Задание 2
-function getRandom(min, max, maxDigits) {
-  if (min > max || min < 0 || max <= 0) {
+function getRandom(min, max, decimals) {
+  if (min >= max || (max-min) < 0 || max < 0 || min < 0) {
     return ('Задан неверный диапазон! Укажите другие числа.');
   }
 
-  const digitsDegree = 10 ** maxDigits;
-  return ~~((Math.random() * (max - min) + min)* digitsDegree) / digitsDegree;
+  const digitsDegree = 10 ** decimals;
+  return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
 }
 getRandom(4, 8, 5);
