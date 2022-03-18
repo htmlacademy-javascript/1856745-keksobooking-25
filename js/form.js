287f163d2330e30660b718fd16f4f3904e75559b
Andrey Volkov <andreyka9678@mail.ru>
2022-03-18 18:43:18 +0500

    7.2

---
+++ js/form.js
@@ -0,0 +1,17 @@
+const adForm = document.querySelector('.ad-form');
+const adFormFieldset = adForm.querySelectorAll('fieldset');
+window.util.setDisabled(adFormFieldset, true);
+
+// Перевод в активное состояние
+const enableActiveState = () => {
+  adForm.classList.remove('ad-form--disabled');
+
+  window.util.setDisabled(adFormFieldset, false);
+};
+
+// Перевод в неактивное состояние
+const enableInactiveState = () => {
+  adForm.classList.add('ad-form--disabled');
+
+  window.util.setDisabled(adFormFieldset, true);
+};
