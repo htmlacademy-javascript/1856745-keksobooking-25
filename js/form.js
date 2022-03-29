cd138fd84fdcbd0de734c304f9e9ebb5fb003043
Andrey Volkov <andreyka9678@mail.ru>
2022-03-30 03:20:39 +0500

    8.1

--- js/form.js
+++ js/form.js
@@ -21,6 +21,58 @@ const enableInactiveState = () => {
   setDisabled(adFormFieldsets, true);
 };
 
+const form = document.querySelector('.ad-form');
+
+const pristine = new Pristine(form, {
+  classTo: 'form__item',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'form__item',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+}, false);
+
+function validateTitle (value) {
+  return value.length >= 30 && value.length <= 100;
+}
+
+pristine.addValidator(
+  form.querySelector('#title'),
+  validateTitle,
+  'От 30 до 100 символов'
+);
+
+function validatePrice (value) {
+  return value >= 5000 && value <= 100000;
+}
+
+pristine.addValidator(
+  form.querySelector('#price'),
+  validatePrice,
+  'Максимальное значение 100 000'
+);
+
+const roomsField = form.querySelector('[name="rooms"]');
+const capacityField = form.querySelector('[name="capacity"]');
+const roomsOption = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
+  '3 комнаты': ['для 3гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100 комнат': ['не для гостей']
+};
+
+function validateRooms () {
+  return roomsOption[roomsField.value].includes(capacityField.value);
+}
+
+pristine.addValidator(roomsField, validateRooms);
+pristine.addValidator(capacityField, validateRooms);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
 export {
   enableActiveState,
   enableInactiveState,
