20dd3ab22e0f39c482b57d18d1f8c5ecac1b90f6
Keks <keks@htmlacademy.ru>
2022-04-01 12:33:07 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/data.js
+++ 7/js/data.js
@@ -13,7 +13,36 @@ export const typeFlat = {
   palace: 'Дворец',
   hotel: 'Отель'
 };
+export const roomToGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+export const offerTypes = {
+  palace: {
+    title: 'Дворец',
+    min: 10000,
+  },
+  flat: {
+    title: 'Квартира',
+    min: 1000,
+  },
+  house: {
+    title: 'Дом',
+    min: 5000,
+  },
+  bungalow: {
+    title: 'Бунгало',
+    min: 0,
+  },
+  hotel: {
+    title: 'Отель ',
+    min: 3000,
+  },
+};
 
+export const MAX_PRICE = 100000;
 const OFFERS_COUNT = 10;
 const COORD_DECIMALS = 5;
 export const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
--- 7/js/form.js
+++ 7/js/form.js
@@ -1,3 +1,8 @@
+import {
+  declineNum
+} from './util.js';
+import { MAX_PRICE, offerTypes, roomToGuests } from './data.js';
+
 // Добавление disabled
 const setDisabled = function (collection, value = true) {
   collection.forEach((item) => {
@@ -21,56 +26,62 @@ const enableInactiveState = () => {
   setDisabled(adFormFieldsets, true);
 };
 
-const form = document.querySelector('.ad-form');
+//Валидация формы
+const adFormElement = document.querySelector('.ad-form');
+const roomsFieldElement = adFormElement.querySelector('[name="rooms"]');
+const capacityFieldElement = adFormElement.querySelector('[name="capacity"]');
+const priceFieldElement = adFormElement.querySelector('[name="price"]');
+const typeFieldElement = adFormElement.querySelector('[name="type"]');
+const PRICE_VALIDATION_PRIORITY = 1000;
 
-const pristine = new Pristine(form, {
+const pristine = new Pristine(adFormElement, {
   classTo: 'form__item',
   errorClass: 'form__item--invalid',
   successClass: 'form__item--valid',
   errorTextParent: 'form__item',
   errorTextTag: 'span',
   errorTextClass: 'form__error'
-}, false);
+});
 
-function validateTitle (value) {
-  return value.length >= 30 && value.length <= 100;
-}
-
-pristine.addValidator(
-  form.querySelector('#title'),
-  validateTitle,
-  'От 30 до 100 символов'
-);
-
-function validatePrice (value) {
-  return value >= 5000 && value <= 100000;
-}
-
-pristine.addValidator(
-  form.querySelector('#price'),
-  validatePrice,
-  'Максимальное значение 100 000'
-);
-
-const roomsField = form.querySelector('[name="rooms"]');
-const capacityField = form.querySelector('[name="capacity"]');
-const roomsOption = {
-  '1 комната': ['для 1 гостя'],
-  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
-  '3 комнаты': ['для 3гостей', 'для 2 гостей', 'для 1 гостя'],
-  '100 комнат': ['не для гостей']
+const setPriceAttributes = () => {
+  const minPrice = offerTypes[typeFieldElement.value].min;
+  priceFieldElement.min = minPrice;
+  priceFieldElement.placeholder = minPrice;
 };
 
-function validateRooms () {
-  return roomsOption[roomsField.value].includes(capacityField.value);
-}
+setPriceAttributes();
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 
-pristine.addValidator(roomsField, validateRooms);
-pristine.addValidator(capacityField, validateRooms);
 
-form.addEventListener('submit', (evt) => {
+pristine.addValidator(
+  adFormElement.querySelector('#title'),
+  validateTitle,
+  'От 30 до 100 символов',
+  PRICE_VALIDATION_PRIORITY,
+  true
+);
+
+const validatePrice = (value) => value >= 5000 && value <= MAX_PRICE;
+
+
+const getPriceMessage = () => `Выберите число между ${priceFieldElement.min} и ${MAX_PRICE}`;
+const validateCapacity = () => roomToGuests[roomsFieldElement.value].includes(capacityFieldElement.value);
+
+const getCapacityMessage = () => {
+  const roos = declineNum(roomsFieldElement.value, 'комнаты', 'комнат');
+  const validGuests = roomToGuests[roomsFieldElement.value];
+  return `Для ${roos} допустимо гостей: ${validGuests.join(', ')}`;
+};
+roomsFieldElement.addEventListener('change', () => pristine.validate(capacityFieldElement));
+
+pristine.addValidator(priceFieldElement, validatePrice, getPriceMessage, PRICE_VALIDATION_PRIORITY, true);
+pristine.addValidator(capacityFieldElement, validateCapacity, getCapacityMessage);
+
+adFormElement.addEventListener('submit', (evt) => {
+  if (pristine.validate()) {
+    console('Успешно');
+  }
   evt.preventDefault();
-  pristine.validate();
 });
 
 export {
